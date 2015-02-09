2b5ff2d5312bd20c7f58c912306e3e64bf03d7a6
Tim Riffe <tim.riffe@gmail.com>
2015-02-08 19:01:30 -0800

    modified file path for new laptop

--- LifetableIncrement/R/CoverPage.R
+++ LifetableIncrement/R/CoverPage.R
@@ -1,7 +1,9 @@
 # 1) copy compiled pdf to 2518.pdf
 
 # 2) append first page of pdf sent by Jana
-system("cd /home/triffe/git/LifetableIncrement/LifetableIncrement && pdftk cover.pdf LifetableIncrement.pdf cat output 2518.pdf")
+system("cd /home/tim/git/LifetableIncrement/LifetableIncrement && pdftk cover.pdf LifetableIncrement.pdf cat output 2518.pdf")
+# old laptop:
+#system("cd /home/triffe/git/LifetableIncrement/LifetableIncrement && pdftk cover.pdf LifetableIncrement.pdf cat output 2518.pdf")
 # one time only:
-#system("pdftk A=/home/triffe/Desktop/2518.pdf cat A1 output /home/triffe/git/LifetableIncrement/LifetableIncrement/cover.pdf")
+#system("pdftk A=/home/tim/Desktop/2518.pdf cat A1 output /home/tim/git/LifetableIncrement/LifetableIncrement/cover.pdf")
 
